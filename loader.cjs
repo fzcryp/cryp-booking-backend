264ff50c5baa01177a5b7967684ec4654644fd9a
fzcryp <fzcryp@gmail.com>
2026-01-20 00:23:59 +0530

    add loader and index remove o.o.o.o for production deployment.

---
+++ loader.cjs
@@ -0,0 +1,4 @@
+async function loadApp() {
+  await import('./src/index.js');
+}
+loadApp();
--- src/index.js
+++ src/index.js
@@ -133,6 +133,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
